f48851d7ba59af77e49afde34039b0ae8655ad68
Hynek Schlawack <schlawack@variomedia.de>
2014-03-13 11:21:04 +0100

    Refactor pkg and prepare for 0.2

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -2,3 +2,12 @@ Credits
 =======
 
 “pem” is written and maintained by Hynek Schlawack.
+
+
+Contributors
+------------
+
+The following wonderful people contributed directly or indirectly to this project:
+
+
+Please add yourself here alphabetically when you submit your first pull request.
--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
+import codecs
+import os
+import re
+
 from setuptools import setup
 
-import pem
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+def find_version(*file_paths):
+    """
+    Build a path from *file_paths* and search for a ``__version__``
+    string inside.
+    """
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='pem',
-    version=pem.__version__,
+    version=find_version("pem.py"),
     description='Parse and split PEM files painlessly.',
-    long_description=(open('README.rst').read() + '\n\n' +
-                      open('HISTORY.rst').read() + '\n\n' +
-                      open('AUTHORS.rst').read()),
-    url='http://github.com/hynek/pem/',
-    license=pem.__license__,
-    author=pem.__author__,
+    long_description=(read('README.rst') + '\n\n' +
+                      read('HISTORY.rst') + '\n\n' +
+                      read('AUTHORS.rst')),
+    url='https://github.com/hynek/pem/',
+    license="MIT",
+    author="Hynek Schlawack",
     author_email='hs@ox.cx',
-    py_modules=['pem'],
+    py_modules=['pem', 'test_pem'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
