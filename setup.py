6542f4bb9e411f4f9bbde310b2b94b932cbad318
Hynek Schlawack <hs@ox.cx>
2015-07-14 11:25:32 +0200

    Remove unused find_version from setup.py

--- setup.py
+++ setup.py
@@ -50,19 +50,6 @@ def read(*parts):
         return f.read()
 
 
-def find_version(*file_paths):
-    """
-    Build a path from *file_paths* and search for a ``__version__``
-    string inside.
-    """
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
 if __name__ == "__main__":
     setup(
         name=NAME,
