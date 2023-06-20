60395b1dfad8e479040010b2edebdef3d7293136
Hynek Schlawack <hs@ox.cx>
2023-06-20 18:17:07 +0200

    Split type-checking
    It's impossible to have a package pass on all versions.

---
+++ tests/typing/api.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2013 Hynek Schlawack <hs@ox.cx>
+#
+# SPDX-License-Identifier: MIT
+
+"""
+This file is used to test the type annotations of the public API. It is NOT
+meant to be executed.
+"""
+
+import pem
+
+
+objs: list[pem.AbstractPEMObject]
+
+objs = pem.parse_file("foo.pem")
+objs = pem.parse("PEM")
+objs = pem.parse(b"PEM")
